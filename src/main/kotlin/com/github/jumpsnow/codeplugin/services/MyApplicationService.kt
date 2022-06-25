933cc191aff74e91b90bdd10c7f2fc10bbef3d46
GitHub Action <action@github.com>
2022-06-25 14:09:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/jumpsnow/codeplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jumpsnow/codeplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jumpsnow.codeplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jumpsnow.codeplugin.MyBundle
 
 class MyApplicationService {
 
